a8db1aae3e970b9376b1bc319176aace4b573132
heungsu0101 < >
2021-08-16 20:59:12 +0900

    N.2581번

--- Baekjoon/BasicMath2/2581.cpp
+++ Baekjoon/BasicMath2/2581.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    int N, M, sum = 0, min = 10001;
+    cin >> M >> N;
+
+    while (M <= N) {
+        int check = 1; //1이면 소수
+
+        if (M == 1) {
+                check = 0;
+        }
+
+        for (int i = 2; i < M; i++) {
+            if ((M % i) == 0) {
+                check = 0;
+                break;
+            }
+        }
+
+        if (check == 1) {
+            if (min > M) {
+                min = M;
+            }
+
+            sum += M;
+        }
+
+        M++;
+    }
     
+    if (sum == 0) {
+        cout << -1;
+        return 0;
+    }
+
+    cout << sum << "\n" << min;
+
 }
