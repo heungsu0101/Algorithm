3fe60a2dbf8d256a545e27deb6f8f6755565ca2b
heungsu0101 < >
2021-08-17 15:01:55 +0900

    4948번_에라토스테네의 체 알고리즘 활용

---
+++ Baekjoon/BasicMath2/4948.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int num[246912];
+
+int primeNumberSieve(int N) {
+
+    int cnt = 0;
+
+    for (int i = 2; i <= 2 * N; i++) {
+        num[i] = i;
+    }
+
+    for (int i = 2; i * i <= 2 * N; i++) {
+        if (num[i] == 0) continue;
+        for (int j = i * i; j <= 2 * N; j += i ) {
+            num[j] = 0;
+        }
+    }
+
+    for (int i = N + 1; i <= 2 * N; i++) {
+        if (num[i] != 0) cnt++;
+    }
+    
+    return cnt;
+}
+
+int main() {
+    vector<int> check;
+    int N; scanf("%d", &N);
+
+    while (N != 0) {
+        check.push_back(primeNumberSieve(N));
+        scanf("%d", &N);
+    }
+
+    int size = check.size();
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", check[i]);
+    }
+}
