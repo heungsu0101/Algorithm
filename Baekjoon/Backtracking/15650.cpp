19a3144959f4b735b8d8c6478bec215440827b41
heungsu0101 < >
2021-08-25 14:33:37 +0900

    15650번

---
+++ Baekjoon/Backtracking/15650.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+bool num[9];
+int order[9];
+
+void permutation(int k, int N, int M) {
+    
+    if (k == M + 1) {
+        for (int i = 1; i <= M; i++) {
+            printf("%d ", order[i]);
+        }
+        printf("\n");
+    } else {
+        for (int i = 1; i <= N; i++) {
+            if (num[i] == true) continue;
+            if (order[k - 1] > i) continue;
+            else {
+                order[k] = i;
+                num[i] = true;
+                permutation(k + 1, N, M);
+                num[i] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    int N, M; scanf("%d %d", &N, &M);
+
+    permutation(1, N, M);
+}
